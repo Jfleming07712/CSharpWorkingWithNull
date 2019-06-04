f1c1d5a16506bc616823f36de98b68cc2f191509
Jfleming <Jfleming07712@gmail.com>
2019-06-04 12:49:37 -0400

    First Module Complete.  Used null check on player values, and displayed them.

---
+++ CSharpWorkingWithNull/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+
+        public int? DaysSinceLastLogin { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public bool? IsNoob { get; set; }
+
+        public PlayerCharacter()
+        {
+            //DateOfBirth = DateTime.MinValue; // magic number
+            //DaysSinceLastLogin = -1;          // magi number
+            //DateOfBirth = null; 
+            //DaysSinceLastLogin = null;          
+        }
+    }
+}
---
+++ CSharpWorkingWithNull/PlayerDisplayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    class PlayerDisplayer
+    {
+        public static void Write(PlayerCharacter player)
+        {
+            if (string.IsNullOrWhiteSpace (player.Name))
+            {
+                Console.WriteLine("Player name is null or all whitespace");
+            }
+            else
+            {
+                Console.WriteLine(player.Name);
+            }
+
+            if (player.DaysSinceLastLogin.HasValue)
+            {
+                Console.WriteLine(player.DaysSinceLastLogin);
+            }
+            else
+            {
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            }
+
+            if (player.DateOfBirth == null)
+            {
+                Console.WriteLine("No date of birth specified");
+            }
+            else
+            {
+                Console.WriteLine(player.DateOfBirth);
+            }
+
+            if (player.IsNoob == null)
+            {
+                Console.WriteLine("Palyer newbie status is unknown");
+            }
+            else if (player.IsNoob == true)
+            {
+                Console.WriteLine("Player is a newbie");
+            }
+            else 
+            {
+                Console.WriteLine("Player is experienced");
+            }
+        }
+    }
+}
